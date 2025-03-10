613cb243eb3f8eb2dfb3eb7912564905f3f3e2fc
sdesaih <samarthdesain@gmail.com>
2025-03-10 16:34:33 +0530

    success

---
+++ Variables.js
@@ -0,0 +1,26 @@
+
+/* 'LET' variable is block scoped */
+let a = 24;
+
+/* 'VAR' variable is function scoped */
+var b = 24;
+
+/* 'CONST' variable is block scoped and cannot be reassigned */
+const PI = 3.14159;
+
+
+
+function demo() {
+
+  let a = 9655510986;
+
+  console.log(`Inner value of A : ${a}`);
+
+  var b = 9655510986;
+
+  console.log(`Inner value of B : ${b}`);
+}
+
+console.log(`Outer valu of A : ${a}`);
+console.log(`Outer valu of B : ${b}`);
+demo();
