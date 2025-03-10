258b01ca607daa0e8a5c93e1f652a0801647ec56
sdesaih <samarthdesain@gmail.com>
2025-03-10 16:57:33 +0530

    succcess

---
+++ Datatypes.js
@@ -0,0 +1,18 @@
+/* Datatypes in Javascript */
+
+let name = "John Doe"; // String
+
+let age = 25; // Number
+
+let isMarried = false; // Boolean
+
+let person = {
+  name: "John D Rockefeller",
+  age: 97,
+}; // Object
+
+let colors = ["red", "green", "blue"]; // Array
+
+let nothing = null; // Null
+
+let notDefined; // Undefined
---
+++ Operators.js
@@ -0,0 +1,59 @@
+/* Arithmetic Operators */
+
+let sum = 5 + 6;
+let sub = 85 - 10;
+let mul = 8 * 2;
+let div = 8 / 4;
+let rem = 52 % 3;
+
+console.log(`Sum : ${sum}`);
+console.log(`Sub : ${sub}`);
+console.log(`Mul : ${mul}`);
+console.log(`Div : ${div}`);
+console.log(`Rem : ${rem}`);
+
+/* Comparison Operators */
+
+let isEqual = 5 == "5";
+let isStrictEqual = 5 === "5";
+let isGreater = 5 > 6;
+let isLess = 5 < 6;
+let isGreaterOrEqual = 5 >= 6;
+let isLessOrEqual = 5 <= 6;
+let isNotEqual = 5 != 6;  
+let isNotStrictEqual = 5 !== 6;
+
+console.log(`Is Equal : ${isEqual}`);
+console.log(`Is Strict Equal : ${isStrictEqual}`);
+console.log(`Is Greater : ${isGreater}`);
+console.log(`Is Less : ${isLess}`);
+console.log(`Is Greater or Equal : ${isGreaterOrEqual}`); 
+console.log(`Is Less or Equal : ${isLessOrEqual}`); 
+console.log(`Is Not Equal : ${isNotEqual}`);
+console.log(`Is Not Strict Equal : ${isNotStrictEqual}`);
+
+/* Logical Operators */
+
+let isTrue = true;
+let isFalse = false;
+let And = true && false;
+let Or = true || false;
+let Not = !true;
+
+console.log(`And : ${And}`);
+console.log(`Or : ${Or}`);
+console.log(`Not : ${Not}`);
+
+/* Assignment Operators */
+
+let a = 5;
+let b = 2;
+
+a += b;
+a -= b;
+a *= b;
+a /= b;
+a %= b;
+
+console.log(`A : ${a}`);
+console.log(`B : ${b}`);
