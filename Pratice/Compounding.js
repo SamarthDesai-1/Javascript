091b1218712059173b59865be7a2e43702bc2ad1
sdesaih <samarthdesain@gmail.com>
2025-04-06 21:21:20 +0530

    success

--- Pratice/Compounding.js
+++ Pratice/Compounding.js
@@ -30,4 +30,65 @@ const calculateCompoundingValue = (amount, interestPerYear, years) => {
 
 
 /** This function invokes the compounding calculator. */
-inputData();
+// inputData()
+
+
+
+const formatNumber = (strNumber) => {
+
+  let x = strNumber;
+  
+  let findPoint = false;
+  let afterDecimal = '';
+  let beforeDecimal = '';
+
+  for (let i = x.length - 1; i >= 0; i--) {
+    if (x[i] == '.') {
+      afterDecimal = x.substring(i);
+      beforeDecimal = x.substring(0, i + 1);
+      findPoint = true;
+      break;
+    }
+  }
+
+  let count = 0;
+  let newFormatBeforeDecimal = '';
+  let startingPoint = findPoint == true ? beforeDecimal.length - 2 : x.length - 1;
+
+  for (let i = startingPoint; i >= 0; i--) {
+    
+    
+    if (count == 3) {
+      newFormatBeforeDecimal += ',';
+      count = 0;
+    }
+
+    newFormatBeforeDecimal += x[i];
+    count++;
+  }
+
+  
+  if (findPoint == true) {
+    newFormatBeforeDecimal = newFormatBeforeDecimal.split('').reverse().join('');
+    newFormatBeforeDecimal += afterDecimal;
+
+  }
+  else {
+    newFormatBeforeDecimal = newFormatBeforeDecimal.split('').reverse().join('');
+  }
+
+  return newFormatBeforeDecimal;
+}
+
+
+
+const process = () => {
+  
+  let principleAmount = Number.parseInt(document.getElementById("amt").value);
+  let interestRate = Number.parseInt(document.getElementById("int").value);
+  let timePeriod = Number.parseInt(document.getElementById("tm").value);
+
+  document.getElementById("compoundedValue").innerHTML = '$' + formatNumber(String(calculateCompoundingValue(principleAmount, interestRate, timePeriod)));
+
+  document.getElementById("compoundedValue").classList.add("format");
+}
